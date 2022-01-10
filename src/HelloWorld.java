b1c565fb35d2aa9158bbf39687bb32037d3d0904
enes3553 <93152484+enes3553@users.noreply.github.com>
2022-01-10 14:45:12 -0500

    work 4 is done and about to be  push to github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,7 @@ public class HelloWorld {
     // Doing awesome work 2!
     // ilk degisikligi yap ondan sonra git in icinden commit e tikala
     // sonrada changes leri secmeklazim ki degisikleri kaydetsin.
+    // Doing awesome  work3
+    // Awesome  workk 3 is done and reasy to be committed
+
 }
