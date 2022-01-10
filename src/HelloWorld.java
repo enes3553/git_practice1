ef855560487c331a9e4c0c75bd7e2c5040c133cf
enes3553 <93152484+enes3553@users.noreply.github.com>
2022-01-10 12:46:20 -0500

    work 4 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     // Doing some work here
     // Doing awesome work 2!
-    // ilk degisikligi yap ondan sonra git in icicnden commit e tikala
+    // ilk degisikligi yap ondan sonra git in icinden commit e tikala
     // sonrada changes leri secmeklazim ki degisikleri kaydetsin.
 }
